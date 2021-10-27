35caf7815d317696acf8c76a1bd440b8e01282ae
Ricardoir <40177989+Ricardoir@users.noreply.github.com>
2021-10-27 08:11:27 -0300

    numero maximo de telefones

--- src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Aluno {
-	
+
 	private CPF cpf;
 	private String nome;
-	
+
 	private Email email;
-	
+
 	private List<Telefone> telefones = new ArrayList<>();
-	
+
 	private String senha;
-	
+
 	public Aluno(CPF cpf, String nome, Email email) {
 		this.cpf = cpf;
 		this.nome = nome;
@@ -21,21 +21,24 @@ public class Aluno {
 	}
 
 	public void adicionarTelefone(String ddd, String numero) {
+		if (telefones.size() == 2) {
+			throw new IllegalArgumentException("Numero maximo de telefones ja atingido!");
+		}
 		this.telefones.add(new Telefone(ddd, numero));
 	}
-	
+
 	public String getCpf() {
 		return cpf.getNumero();
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public String getEmail() {
 		return email.getEndereco();
 	}
-	
+
 	public List<Telefone> getTelefones() {
 		return telefones;
 	}
@@ -43,11 +46,5 @@ public class Aluno {
 	public String getSenha() {
 		return senha;
 	}
-	
+
 }
-
-
-
-
-
-
